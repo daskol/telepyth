9033b164cb2a3ae708b3bb00352de512316b52a9
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2017-04-28 21:23:08 +0300

    Implement client.

---
+++ telepyth/client.py
@@ -0,0 +1,42 @@
+#   encoding: utf8
+#   client.py
+
+from sys import exc_info, stderr
+from urllib.request import Request, urlopen
+
+from .version import __user_agent__, __version__
+
+
+class TelePythClient(object):
+
+    DEBUG_URL = 'http://localhost:8080/api/notify/'
+    BASE_URL = 'https://daskol.xyz/api/notify/'
+
+    def __init__(self, token, base_url=None, debug=False):
+        self.token = token
+        self.base_url = base_url or TelePythClient.BASE_URL
+
+        if debug:
+            self.base_url = TelePythClient.DEBUG_URL
+
+    def __call__(self, text, markdown=True):
+        url = self.base_url + self.token
+
+        req = Request(url, method='POST')
+        req.add_header('Content-Type', 'plain/text; encoding=utf-8')
+        req.add_header('User-Agent', __user_agent__ + '/' + __version__)
+        req.data = text.encode('utf8')
+
+        try:
+            res = urlopen(req)
+
+            if res.getcode() != 200:
+                lines = '\n'.join(res.readlines())
+                msg = '[%d] %s: %s' %(res.getcode(), res.reason, lines)
+                print(msg, file=stderr)
+
+            return res.getcode()
+        except Exception as e:
+            print('During request exception was raised:', e, file=stderr)
+            print(exc_info(), file=stderr)
+            return None
---
+++ telepyth/version.py
@@ -0,0 +1,5 @@
+#   encoding: utf8
+#   version.py
+
+__user_agent__ = 'telepyth'
+__version__ = '0.0.0'
