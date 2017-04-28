eaedcdd456aef3b06c81706520e06c277885568c
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2017-04-29 02:39:45 +0300

    Format storage tests.

--- storage_test.go
+++ storage_test.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-    "testing"
-    "io/ioutil"
+	"io/ioutil"
+	"testing"
 )
 
 func TestStorage(t *testing.T) {
-    file, err := ioutil.TempFile("", "boltdb-")
-    storage, err := NewStorage(file.Name())
-    defer storage.Close()
+	file, err := ioutil.TempFile("", "boltdb-")
+	storage, err := NewStorage(file.Name())
+	defer storage.Close()
 
-    // let Pavel Durov check it out
-    user := &User{
-        Id: 1,
-        FirstName: "Pavel",
-        LastName: "Durov",
-        UserName: "durov",
-    }
+	// let Pavel Durov check it out
+	user := &User{
+		Id:        1,
+		FirstName: "Pavel",
+		LastName:  "Durov",
+		UserName:  "durov",
+	}
 
-    // place user onto index
-    token, err := storage.InsertUser(user)
+	// place user onto index
+	token, err := storage.InsertUser(user)
 
-    if err != nil {
-        t.Error(err)
-    }
+	if err != nil {
+		t.Error(err)
+	}
 
-    if len(token) == 0 {
-        t.Error("wrong token")
-    }
+	if len(token) == 0 {
+		t.Error("wrong token")
+	}
 
-    // query user by token
-    usr, err := storage.SelectUserBy(token)
+	// query user by token
+	usr, err := storage.SelectUserBy(token)
 
-    if err != nil {
-        t.Error(err)
-    }
+	if err != nil {
+		t.Error(err)
+	}
 
-    if (usr.Id != user.Id || usr.LastName != user.LastName ||
-        usr.FirstName != user.FirstName || usr.UserName != user.UserName) {
-            t.Error("wrong result: ", usr)
-    }
+	if usr.Id != user.Id || usr.LastName != user.LastName ||
+		usr.FirstName != user.FirstName || usr.UserName != user.UserName {
+		t.Error("wrong result: ", usr)
+	}
 
-    // query token by user
-    tok, err := storage.SelectTokenBy(user)
+	// query token by user
+	tok, err := storage.SelectTokenBy(user)
 
-    if err != nil {
-        t.Error(err)
-    }
+	if err != nil {
+		t.Error(err)
+	}
 
-    if tok != token {
-        t.Error("wrong token: ", tok)
-    }
+	if tok != token {
+		t.Error("wrong token: ", tok)
+	}
 }
