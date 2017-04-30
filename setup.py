7e9fdc475c881f1876afdda3fd03e3ab3b94e563
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2017-04-30 14:55:16 +0300

    Add initial setup script and requirements.

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#   encoding: utf8
+#   setup.py
+"""TelePyth: Telegram notifications in Python.
+"""
+
+from setuptools import find_packages, setup
+
+DOCLINES = (__doc__ or '').split('\n')
+
+CLASSIFIERS = """\
+Development Status :: 4 - Beta
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Topic :: Software Development
+Topic :: Scientific/Engineering
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Operating System :: Unix
+Operating System :: MacOS
+"""
+
+PLATFORMS = [
+    'Windows',
+    'Linux',
+    'Solaris',
+    'Mac OS-X',
+    'Unix'
+]
+
+MAJOR = 0
+MINOR = 0
+PATCH = 0
+
+VERSION = '{0:d}.{1:d}.{2:d}'.format(MAJOR, MINOR, PATCH)
+
+
+setup(name='telepyth',
+      version=VERSION,
+      description = DOCLINES[0],
+      long_description = '\n'.join(DOCLINES[2:]),
+      url='https://github.com/daskol/telepyth',
+      download_url='https://github.com/daskol/telepyth/tarball/v' + VERSION,
+      author='Daniel Bershatsky',
+      author_email='daniel.bershatsky@skolkovotech.ru',
+      maintainer='Daniel Bershatsky',
+      maintainer_email='daniel.bershatsky@skolkovotech.ru',
+      license='MIT',
+      platforms=PLATFORMS,
+      classifiers=[line for line in CLASSIFIERS.split('\n') if line],
+      packages=find_packages())
