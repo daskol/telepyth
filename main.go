12cce38b78a3afbe878e572b7c32a7518fb7f251
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2017-05-01 03:07:37 +0300

    Load config from toml-file.

--- main.go
+++ main.go
@@ -2,33 +2,55 @@ package main
 
 import (
 	"flag"
-	_ "github.com/BurntSushi/toml"
+	"github.com/BurntSushi/toml"
 	"github.com/daskol/telepyth/srv"
 	"log"
 )
 
 var storage *srv.Storage
 
+type Config struct {
+	Token   string `toml:"token"`
+	Storage string `toml:"storage"`
+	Polling bool   `toml:"polling"`
+	Timeout int    `toml:"timeout"`
+}
+
 func main() {
 	configPath := flag.String("config", "", "Path to toml config file.")
 	token := flag.String("token", "", "A unique authentication token.")
-	dbPath := flag.String("database", "var/bolt.data",
+	dbPath := flag.String("database", "bolt.db",
 		"Create or open a database at the given path.")
+	polling := flag.Bool("polling", false, "Use long polling to get updates")
+	timeout := flag.Int("timeout", 30, "Timeout in seconds for long polling.")
 
 	flag.Parse()
 
-	log.Println("load config from " + *configPath)
-	log.Println("open database at " + *dbPath)
+	config := &Config{
+		Token:   *token,
+		Storage: *dbPath,
+		Polling: *polling,
+		Timeout: *timeout,
+	}
 
-	if db, err := srv.NewStorage(*dbPath); err != nil {
+	if len(*configPath) != 0 {
+		log.Println("load config from " + *configPath)
+		if _, err := toml.DecodeFile(*configPath, config); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	log.Println("open database at " + config.Storage)
+
+	if db, err := srv.NewStorage(config.Storage); err != nil {
 		log.Fatal(err)
 	} else {
 		storage = db
 		defer storage.Close()
 	}
 
-	log.Println("use token " + *token)
-	api := srv.New(*token)
+	log.Println("use token " + config.Token)
+	api := srv.New(config.Token)
 
 	if me, err := api.GetMe(); err != nil {
 		log.Fatal("exit: ", err)
