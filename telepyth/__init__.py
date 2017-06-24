78bebaa2902636e33409591675b1bede6c359aad
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2017-06-24 15:53:32 +0300

    Add alias to TelePythClient which will be deprecated in the future.

--- telepyth/__init__.py
+++ telepyth/__init__.py
@@ -4,5 +4,8 @@
 from telepyth.client import TelePythClient
 from telepyth.utils import is_interactive
 
+
+TelepythClient = TelePythClient  # make alias to origin definition
+
 if is_interactive():
     from telepyth.magics import TelePythMagics
